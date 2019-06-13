97393a5cff73d30b426f8f1f36361c0cb59180cf
Theriley106 <christopherlambert106@gmail.com>
2019-06-13 19:34:12 -0400

    Updated app to remove random stuff

--- app.py
+++ app.py
@@ -9,66 +9,18 @@ import json
 from flask import Flask, request
 
 app = Flask(__name__)
-bot_id = "REPLACE THIS WITH YOUR BOT ID ONCE BOT IS ADDED TO THE CHAT"
 
 # Called whenever the app's callback URL receives a POST request
 # That'll happen every time a message is sent in the group
 @app.route('/', methods=['POST'])
 def webhook():
 	# 'message' is an object that represents a single GroupMe message.
+	print(request.get_json())
 	main.check_message(request.get_json())
 	print(request.get_json())
 	#print message
 	print("AAYYYYYYYY")
 	return "ok", 200
-
-################################################################################
-
-# Send a message in the groupchat
-def reply(msg):
-	url = 'https://api.groupme.com/v3/bots/post'
-	data = {
-		'bot_id'		: bot_id,
-		'text'			: msg
-	}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
-
-# Send a message with an image attached in the groupchat
-def reply_with_image(msg, imgURL):
-	url = 'https://api.groupme.com/v3/bots/post'
-	urlOnGroupMeService = upload_image_to_groupme(imgURL)
-	data = {
-		'bot_id'		: bot_id,
-		'text'			: msg,
-		'picture_url'		: urlOnGroupMeService
-	}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
-
-# Uploads image to GroupMe's services and returns the new URL
-def upload_image_to_groupme(imgURL):
-	imgRequest = requests.get(imgURL, stream=True)
-	filename = 'temp.png'
-	postImage = None
-	if imgRequest.status_code == 200:
-		# Save Image
-		with open(filename, 'wb') as image:
-			for chunk in imgRequest:
-				image.write(chunk)
-		# Send Image
-		headers = {'content-type': 'application/json'}
-		url = 'https://image.groupme.com/pictures'
-		files = {'file': open(filename, 'rb')}
-		payload = {'access_token': 'eo7JS8SGD49rKodcvUHPyFRnSWH1IVeZyOqUMrxU'}
-		r = requests.post(url, files=files, params=payload)
-		imageurl = r.json()['payload']['url']
-		os.remove(filename)
-		return imageurl
-
-# Checks whether the message sender is a bot
-def sender_is_bot(message):
-	return message['sender_type'] == "bot"
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', port=8000, debug=True)
