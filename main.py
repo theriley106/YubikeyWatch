17cf702ac1dd5f4b01258b48342d6c7a298f9baa
Theriley106 <christopherlambert106@gmail.com>
2019-06-13 18:30:25 -0400

    Added main upload to groupme

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import shutil
 import json
 import os
 
+MY_BOT_ID = "14b11096b7b9e36470dfc5e083"
+MY_ACCESS_TOKEN = "jZCDAeFXqOxmo3IFB6bhmcqnDj5d5WZ90sobykeG"
+DEFAULT_PIC = "https://via.placeholder.com/512"
+
 def is_yubikey(stringVal):
 	return bool(re.match("^(c{6}\w{38})+$", stringVal))
 
@@ -27,62 +31,84 @@ callBack = {
     "group_id": "47732680"
 }
 
-def download_image(url, saveAs):
+def download_image(url):
+	# This downloads the profile picture from GroupMe
+	if url == None:
+		url = DEFAULT_PIC
 	response = requests.get(url)
-	img = Image.open(StringIO(response.content))
-	img = img.resize((1200, 800))
-	img.save("testing.jpeg")
+	return Image.open(StringIO(response.content))
 
-def create_image(callBack):
-	img = Image.open("image.png")
-	#draw = ImageDraw.Draw(img)
-	# font = ImageFont.truetype(<font-file>, <font-size>)
-	font = ImageFont.truetype("arial.ttf", 22)
-	# draw.text((x, y),"Sample Text",(r,g,b))
-	old_size = img.size
-
-	new_size = (300, 200)
-	new_im = Image.new("RGB", new_size, (255, 255, 255))   ## luckily, this is already black!
-
-
-	draw = ImageDraw.Draw(new_im)
-
-	draw.text((8, 38),"{}: ".format(callBack['name'].split(" ")[0]),(0,0,0),font=font)
-	new_im = new_im.resize((1200, 800))
-
-	new_im.paste(img, ((0, 0)))
-	#new_im.show()
-	response = requests.get(callBack.get("avatar_url"))
-	profile = Image.open(StringIO(response.content))
-	profile = profile.resize((900,500))
-	font = ImageFont.truetype("arial.ttf", 40)
-	# draw.text((x, y),"Sample Text",(r,g,b))
-
-	draw = ImageDraw.Draw(profile)
-
-	draw.text((20, 440),'"' + callBack['text'] + '"',(255,255,0),font=font)
-	new_im.paste(profile, ((30, 270)))
-	new_im.save('sample-out.png')
-
+def pretend_to_save_to_file(img):
+	# Takes in an Image object
+	# Returns an opened "File"
 	output = StringIO()
-	format = 'PNG' # or 'JPEG' or whatever you want
-	new_im.save(output, format)
+	img.save(output, "PNG")
 	data = output.getvalue()
 	output.close()
+	return data
 
-
+def upload_an_image_to_group_me(img):
+	# Takes in a Image object
+	# Returns a string containing a groupme image url
+	data = pretend_to_save_to_file(img)
 	headers = {
-	    'X-Access-Token': 'jZCDAeFXqOxmo3IFB6bhmcqnDj5d5WZ90sobykeG',
+	    'X-Access-Token': MY_ACCESS_TOKEN,
 	    'Content-Type': 'image/jpeg',
 	}
 	response = requests.post('https://image.groupme.com/pictures', headers=headers, data=data)
-	#os.system("curl 'https://image.groupme.com/pictures'" + ' -X POST -H "X-Access-Token: jZCDAeFXqOxmo3IFB6bhmcqnDj5d5WZ90sobykeG" -H "Content-Type: image/jpeg" --data-binary @sample-out.png > t.txt')
+	print response.text
+	return response.json()['payload']['picture_url']
 
-	a = json.load(open('t.txt'))
-	print a
-	a = a['payload']['picture_url']
-	x = '''{"bot_id"  : "14b11096b7b9e36470dfc5e083","text"    : "Hello world","attachments" : [{"type"  : "image","url"   : "''' + a + '''"}]}'''
-	os.system('''curl -d '{}' https://api.groupme.com/v3/bots/post'''.format(x))
+
+def post_image_to_group_me(img):
+	# Takes in an Image object
+	# Returns nothing
+	data = {"bot_id"  : MY_BOT_ID,
+	  "text"    : "Hello worldz",
+	  "attachments" : [
+	    {
+	      "type"  : "image",
+	      "url"   : upload_an_image_to_group_me(img)
+	    }
+	  ]
+	}
+	response = requests.post('https://api.groupme.com/v3/bots/post', json=data)
+
+
+
+def create_image(callBack):
+	# This function creates the meme image
+	# Takes in the data sent as a post request from GroupMe
+	img = Image.open("image.png")
+	# Opens up the meme template
+	font = ImageFont.truetype("arial.ttf", 22)
+	# Sets the font size and font type for the top of the meme
+	old_size = img.size
+	# This is the original size of the meme image
+	new_im = Image.new("RGB", (300, 200), (255, 255, 255))   ## luckily, this is already black!
+	# Creates a new image with a blank border
+	draw = ImageDraw.Draw(new_im)
+	# Sets up the picture to add the text to it
+	draw.text((8, 38),"{}: ".format(callBack['name'].split(" ")[0]),(0,0,0),font=font)
+	# Writes the persons name on top of the meme
+	new_im = new_im.resize((1200, 800))
+	# Resizes the text to make that blurry look
+	new_im.paste(img, ((0, 0)))
+	#new_im.show()
+	profile = download_image(callBack.get("avatar_url")).resize((900,500))
+	# Downloads and resizes the image for the meme
+	font = ImageFont.truetype("arial.ttf", 40)
+	# Sets the font size and font type for the bottom of the meme
+	draw = ImageDraw.Draw(profile)
+	# Sets up the picture to add the text to it
+	draw.text((20, 440),'"' + callBack['text'] + '"',(255,255,0),font=font)
+	# Adds the yubikey to the top of the meme before the profile picture
+	new_im.paste(profile, ((30, 270)))
+	# Pastes the profile picture on top of the meme
+	#new_im.save('sample-out.png')
+	# ^ This saves the profile picture
+	post_image_to_group_me(new_im)
+	# Posts the image to the GroupMe
 
 def check_message(callBack):
 	if is_yubikey(callBack["text"]):
