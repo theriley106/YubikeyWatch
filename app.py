d1e30bcdb0c53c14e54b1fd3b810c0082ac22d6e
Theriley106 <christopherlambert106@gmail.com>
2019-06-18 20:17:42 -0400

    updated app

--- app.py
+++ app.py
@@ -11,15 +11,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # Called whenever the app's callback URL receives a POST request
-# That'll happen every time a message is sent in the group
 @app.route('/', methods=['POST'])
 def webhook():
 	# 'message' is an object that represents a single GroupMe message.
 	print(request.get_json())
 	main.check_message(request.get_json())
 	print(request.get_json())
-	#print message
-	print("AAYYYYYYYY")
 	return "ok", 200
 
 if __name__ == '__main__':
